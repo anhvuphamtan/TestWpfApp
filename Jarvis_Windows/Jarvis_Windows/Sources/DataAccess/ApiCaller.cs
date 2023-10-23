704f7e5c2dfefb7bcee63825447a566173a71144
TranTrongDai <trongdaitran0903@gmail.com>
2023-10-24 01:41:16 +0700

    feat(refartor): reset prject's structure and dependencies

---
+++ Jarvis_Windows/Jarvis_Windows/Sources/DataAccess/ApiCaller.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarvis_Windows.Sources.DataAccess
+{
+    public sealed class ApiCaller
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly ApiCaller instance = new ApiCaller();
+
+        private ApiCaller()
+        {
+            // Private constructor to prevent external instantiation
+        }
+
+        public static ApiCaller Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public async Task<string?> ConnectToDatabaseAsync(string apiUrl)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error connecting to database: {ex.Message}");
+                return null;
+            }
+        }
+
+        public async Task<string?> CallApi(string apiUrl, Dictionary<string, string> headers, string requestBody)
+        {
+            try
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, apiUrl);
+
+                // Add headers to the request
+                if (headers != null)
+                {
+                    foreach (var header in headers)
+                    {
+                        request.Headers.Add(header.Key, header.Value);
+                    }
+                }
+
+                // Add request body
+                if (!string.IsNullOrEmpty(requestBody))
+                {
+                    request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException ex)
+            {
+                // Handle exception
+                Console.WriteLine($"Error calling API: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
---
+++ Jarvis_Windows/Jarvis_Windows/Sources/Untitiles/WindowsAPI/WindowsAPI.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarvis_Windows.Sources.Untitiles.WindowsAPI
+{
+    public class WindowsAPI
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="text"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern int SetWindowText(IntPtr hWnd, string text);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetFocus();
+
+        /// <summary>
+        /// Helper function to get the text length of a window's title
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hWndParent"></param>
+        /// <param name="callback"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern bool EnumChildWindows(IntPtr hWndParent, EnumWindowProc callback, IntPtr lParam);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        public delegate bool EnumWindowProc(IntPtr hWnd, IntPtr lParam);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+    }
+}
